b5b81abb6c00a6519fcb709b44af44fbec0c816a
Rusu Andrei-Cristian <rusuandreicristian32@gmail.com>
2023-04-15 18:56:46 +0300

    add interpolationStrings

--- packages/cli/src/templates/front-end/src/startup/core.ts
+++ packages/cli/src/templates/front-end/src/startup/core.ts
@@ -3,12 +3,14 @@ import { ReactPackage } from '@ts-chimera/react';
 import { AppPackage } from './app';
 
 import * as translations from '@src/translations';
+import { interpolationStrings } from '@src/defs';
 
 export const core = new Core({
   packages: [
     new ReactPackage({
       i18n: {
         translations,
+        interpolationStrings,
       },
     }),
     new AppPackage(),
