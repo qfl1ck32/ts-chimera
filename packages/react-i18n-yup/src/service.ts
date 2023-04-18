63113e16b04650fefc8fe4093d41a60b518bdee9
Rusu Andrei-Cristian <rusuandreicristian32@gmail.com>
2023-04-18 09:32:27 +0300

    fix; bump v

--- packages/react-i18n-yup/src/service.ts
+++ packages/react-i18n-yup/src/service.ts
@@ -1,10 +1,6 @@
 import { Injectable, Inject, InjectToken } from '@ts-phoenix/di';
 import { EventManager } from '@ts-phoenix/event-manager';
-import {
-  LocaleChangedEvent,
-  PACKAGE_CONFIG_TOKEN as I18N_PACKAGE_CONFIG_TOKEN,
-  PackageConfigType as I18nPackageConfigType,
-} from '@ts-phoenix/react-i18n';
+import { LocaleChangedEvent } from '@ts-phoenix/react-i18n';
 import * as yup from 'yup';
 
 import { PACKAGE_CONFIG_TOKEN } from './config';
@@ -19,27 +15,23 @@ export class Yup {
   constructor(
     @Inject(EventManager) private eventManager: EventManager,
     @InjectToken(PACKAGE_CONFIG_TOKEN) private config: PackageConfigType,
-    @InjectToken(I18N_PACKAGE_CONFIG_TOKEN)
-    private i18nConfig: I18nPackageConfigType,
   ) {
     this.eventManager.addListener({
       event: LocaleChangedEvent,
       handler: this.onLocaleChange,
     });
+  }
 
-    yup.setLocale(
-      translationsWithoutPaths[this.i18nConfig.defaultLocale as never],
-    );
+  private get translationPaths() {
+    return this.config.usePathsInTranslations
+      ? translationsWithPaths
+      : translationsWithoutPaths;
   }
 
   private onLocaleChange = (event: LocaleChangedEvent) => {
     const locale = event.data!.locale;
 
-    yup.setLocale(
-      (this.config.usePathsInTranslations
-        ? translationsWithPaths
-        : translationsWithoutPaths)[locale as never],
-    );
+    yup.setLocale(this.translationPaths[locale as never]);
   };
 }
 
