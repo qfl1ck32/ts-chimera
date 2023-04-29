0515d62e90ea802653eab925d4d96823674e6324
Rusu Andrei-Cristian <rusuandreicristian32@gmail.com>
2023-04-29 13:29:35 +0300

    name

--- packages/react-i18n-dayjs/src/package.ts
+++ packages/react-i18n-dayjs/src/package.ts
@@ -5,7 +5,7 @@ import { I18nPackage, LocaleChangedEvent } from '@ts-phoenix/react-i18n';
 import { I18nDayjsServiceToken } from './constants';
 import { I18nDayjsService } from './service';
 
-export class DayjsPackage extends Package {
+export class I18nDayjsPackage extends Package {
   getDependencies() {
     return [I18nPackage];
   }
