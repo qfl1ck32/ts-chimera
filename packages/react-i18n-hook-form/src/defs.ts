dc4b662e6b6f90435d6d946c044632d0484090ce
Rusu Andrei-Cristian <hello@andreirusu.dev>
2023-07-04 10:52:38 +0300

    (fix): type

--- packages/react-i18n-hook-form/src/defs.ts
+++ packages/react-i18n-hook-form/src/defs.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line import/named
+import { InferType } from 'yup';
+
 type ISchema<T> = () => T;
 
-export type GetSchemaType<T extends ISchema<any>> = ReturnType<T>;
+export type GetSchemaType<T extends ISchema<any>> = InferType<ReturnType<T>>;
