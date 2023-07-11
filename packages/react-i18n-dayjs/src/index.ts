6085e0de13e46cbe8ec3b4783e759534e1b5db38
Rusu Andrei-Cristian <hello@andreirusu.dev>
2023-07-11 20:25:38 +0300

    (fix): exports

--- packages/react-i18n-dayjs/src/index.ts
+++ packages/react-i18n-dayjs/src/index.ts
@@ -2,3 +2,7 @@ export * from './service';
 export * from './package';
 export * from './defs';
 export * from './constants';
+
+import dayjs from 'dayjs';
+
+export { dayjs };
--- packages/react-i18n-dayjs/src/service.ts
+++ packages/react-i18n-dayjs/src/service.ts
@@ -1,16 +1,12 @@
 import { Service } from '@ts-phoenix/core';
 import { LocaleChangedEvent } from '@ts-phoenix/react-i18n';
-import * as dayjs from 'dayjs';
+import { locale } from 'dayjs';
 
 import { II18nDayjsService } from './defs';
 
 @Service()
 export class I18nDayjsService implements II18nDayjsService {
   public onLocaleChange = (event: LocaleChangedEvent) => {
-    const locale = event.data.locale;
-
-    dayjs.locale(locale);
+    locale(event.data.locale);
   };
 }
-
-export { dayjs };
